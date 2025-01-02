654664e5e15256e9692ffbec64e1992c120d0168
visitassaquib <visitassaquib@gmail.com>
2025-01-02 16:02:19 +0530

    Update Test.java
    added isPalindrome method

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,6 +1,13 @@
 package org.example;
 
 public class Test {
+    public static boolean isPalindrome(String str){
+        if(str.length()>3){
+             return true;
+        }else{
+            return false;
+        }
+    }
     public static void main(String[] args) {
         System.out.println("hi");
         //started to work from visitassaquib
